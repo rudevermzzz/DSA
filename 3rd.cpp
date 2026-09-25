b574b5bb0e5a09c53ccaf78a6577d168e6035da4
Rudra Verma <rudra9379@gmail.com>
2026-09-25 10:29:18 +0530

    Add files via upload

---
+++ 3rd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cin>> n;
+    vector<int> arr(n);
+    for(int i = 0; i <n; i++){
+        cin>> arr[i];
+    }
+    int maxfirst = 0;
+    for(int i =0 ; i<n; i++){
+        if(arr[i]>arr[maxfirst]){
+            maxfirst = i;
+            
+        }
+    }
+    cout<<maxfirst<<endl;
+}
