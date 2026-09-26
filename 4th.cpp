1f05cb020982f3830df1b9e1d35a0721b6591f28
Rudra Verma <rudra9379@gmail.com>
2026-09-26 16:35:25 +0530

    Add files via upload

---
+++ 4th.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i = 0; i<n ; i++){
+        cin>>arr[i];
+    }
+    int marks;
+    cin>>marks;
+    int count = 0;
+    for(int i = 0; i<n; i++){
+        if(arr[i] == marks)
+        count ++;
+    }
+    cout<<count<<endl;
+
+}
